a4b1065d79b59b72d0e2df7927f543c2e9da344c
iamfeysal <iamfeysal@gmail.com>
2020-04-09 22:50:23 +0300

    refactor

--- famstudios/urls.py
+++ famstudios/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),  # < here
     path('__debug__/', include(debug_toolbar.urls)),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
