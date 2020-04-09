2a1c11216cdbaa63a528ec0ce8c02d1803e97aa5
iamfeysal <iamfeysal@gmail.com>
2020-04-09 22:47:45 +0300

    add static and ckeditor and debug toolbar urls

--- famstudios/urls.py
+++ famstudios/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),  # < here
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 
