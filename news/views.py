d8e41b62a9b004dac425bc3a8e9e73c74e8317a8
iamfeysal <iamfeysal@gmail.com>
2020-04-11 00:30:16 +0300

    refoctor views

--- news/views.py
+++ news/views.py
@@ -18,12 +18,20 @@ def post_detail(request, id):
     return render(request, 'post_detail.html', data)
 
 
-# def latest_view(request):
-#     posts = Post.published.all()
-#     categories = Category.objects.all()
-#     print(categories)
-#     data = {'posts': posts, 'categories': categories}
-#     return render(request, 'index.html', data)
+def latest_view(request):
+    posts = Post.published.all()
+    data = {'posts': posts, }
+    return render(request, 'index.html', data)
+def search_view(request):
+    r_search = request.POST['search']
+    posts = Post.objects.filter(Q(
+        title__icontains=r_search) | Q(
+        slug__icontains=r_search) | Q(
+        description__icontains=r_search)
+                                )
+    data = {'posts': posts}
+
+    return render(request, 'search.html', data)
 
 # class HomeView(ListView):
 #     template_name = 'index.html'
@@ -39,32 +47,23 @@ def post_detail(request, id):
 #         # This will show your 3 latest posts you can add accordingly
 #         return context
 
-class HomeView(ListView):
-    template_name = 'index.html'
-    model = Post
+# class HomeView(ListView):
+#     template_name = 'index.html'
+#     model = Post
+#
+#     # context_object_name = 'all_categs'
+#
+#     def get_queryset(self):
+#         self.category = get_object_or_404(Category)
+#         posts = Post.published.all()
+#         print(posts)
+#         return posts.filter(category=self.category)
+#
+#     def get_context_data(self,**kwargs):
+#         context = super(HomeView, self).get_context_data(**kwargs)
+#         context['posts'] = Post.published.all()
+#         # This will show your 3 latest posts you can add accordingly
+#         return context
 
-    # context_object_name = 'all_categs'
-
-    def get_queryset(self):
-        self.category = get_object_or_404(Category)
-        posts = Post.published.all()
-        print(posts)
-        return posts.filter(category=self.category)
-
-    def get_context_data(self,**kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['posts'] = Post.published.all()
-        # This will show your 3 latest posts you can add accordingly
-        return context
 
 
-def search_view(request):
-    r_search = request.POST['search']
-    posts = Post.objects.filter(Q(
-        title__icontains=r_search) | Q(
-        slug__icontains=r_search) | Q(
-        description__icontains=r_search)
-                                )
-    data = {'posts': posts}
-
-    return render(request, 'search.html', data)
