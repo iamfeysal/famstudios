d724b994ba17614417652b058c0e4fb09dc7d107
iamfeysal <iamfeysal@gmail.com>
2020-04-11 02:14:40 +0300

    removed some urls from urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include, re_path
-from .views import archive_view, post_detail, search_view, \
-    category_list, category_detail, latest_view
+from .views import archive_view, post_detail, search_view, latest_view
 
 urlpatterns = [
     path('', latest_view, name='home'),
     path('archive/', archive_view, name='archive'),
     path('post/<int:id>', post_detail, name='post_detail'),
-    re_path(r'^category$', category_list, name='category_list'),
-    re_path(r'^category/(?P<pk>\d+)/$', category_detail,
-            name='category_detail'),
+    # re_path(r'^category$', category_list, name='category_list'),
+    # re_path(r'^category/(?P<pk>\d+)/$', category_detail,
+    #         name='category_detail'),
     path('search_result/', search_view, name='search'),
 ]
