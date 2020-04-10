c5a31151e189446f5b9959729d0ed05cc0da03bb
iamfeysal <iamfeysal@gmail.com>
2020-04-10 23:33:04 +0300

    rewrite category model

--- news/models.py
+++ news/models.py
@@ -13,10 +13,25 @@ class PublishedManager(models.Manager):
 
 
 class Category(models.Model):
-    name = models.CharField(db_index=True, max_length=150, primary_key=True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True,
+                               related_name='children',
+                               on_delete=models.CASCADE)
+
+    class Meta:
+        # enforcing that there can not be two categories under a parent with
+        # same slug
+
+        # __str__ method elaborated later in post.  use __unicode__ in place of
+
+        # __str__ if you are using python 2
+
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = "categories"
 
     def __str__(self):
-        return self.name
+        full_path = [self.name]
 
 
 class Post(models.Model):
