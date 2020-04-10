c9f061778669720260ab35087d3780a59b33f813
iamfeysal <iamfeysal@gmail.com>
2020-04-11 00:38:19 +0300

    import re_path

--- news/urls.py
+++ news/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('archive/', archive_view, name='archive'),
     path('post/<int:id>', post_detail, name='post_detail'),
-    path(r'^category$', category_list, name='category_list'),
-    path(r'^category/(?P<pk>\d+)/$', category_detail,
+    re_path(r'^category$', category_list, name='category_list'),
+    re_path(r'^category/(?P<pk>\d+)/$', category_detail,
         name='category_detail'),
     path('search_result/', search_view, name='search'),
 ]
