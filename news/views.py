8f0e019390021603e8989ecff97886e2dc742aa1
iamfeysal <iamfeysal@gmail.com>
2020-04-12 01:45:55 +0300

    add tag

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from taggit.models import Tag
 
 from .models import Post, Category
 from django.db.models import Q, Count
@@ -46,14 +47,33 @@ def post_detail(request, id):
 #         context['category'] = self.category
 #         return context
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts = Post.published.all()
     category_list_count = Post.objects.annotate(num_category=Count('category'))
     # category_list_count = Post.objects.annotate(num_category=Count(
     # 'category'))
 
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts = posts.filter(tags__in=[tag])
+
+    # paginator = Paginator(posts, 10)  # 3 posts in each page
+    # page = request.GET.get('page')
+    #
+    # try:
+    #     posts = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # If page is not an integer deliver the first page
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     # If page is out of range deliver last page of results
+    #     posts = paginator.page(paginator.num_pages)
+
     context = {
         'posts': posts,
+        'tag': tag,
         'category_list_count': category_list_count,
     }
     return render(request, "index.html", context)
