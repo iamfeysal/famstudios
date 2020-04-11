f1d2952d834b2370d2407086cb2d041b52a5792d
iamfeysal <iamfeysal@gmail.com>
2020-04-12 01:46:44 +0300

    add tag urls

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,8 @@ from .views import archive_view, post_detail, search_view, post_list
 
 urlpatterns = [
     path('', post_list, name='home'),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', post_list,
+            name='post_list_by_tag'),
     path('archive/', archive_view, name='archive'),
     path('post/<int:id>', post_detail, name='post_detail'),
     # re_path(r'^post/cat/(?P<id>\d+)/$', PostCategory.as_view(),
