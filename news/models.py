556013d7276adba3d724aeac8225a4ad17bbc586
iamfeysal <iamfeysal@gmail.com>
2020-04-12 04:58:21 +0300

    reformating templates

--- news/models.py
+++ news/models.py
@@ -30,9 +30,6 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('postcategory', args=[self.title, self.slug])
 
-    def get_posts(self):
-        return Post.objects.filter(category__title=self.title)
-
 
 class Post(models.Model):
     STATUS_CHOICES = (
